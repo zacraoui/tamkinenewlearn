66b998b53a028f1f70f1d996a3c00807aa217128
zacraoui <66187715+zacraoui@users.noreply.github.com>
2021-09-09 13:11:19 +0100

    First commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,18 +1,38 @@
-
+  <link rel="stylesheet" type="text/css" href="{{ asset('css\owl.carousel.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css\app.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css\owl.theme.default.css') }}">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">    
-<div class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+<html>
+  <header>
+  <script>
+window.onscroll = function() {myFunction()};
 
-                    <div class="navbar-header">
+var header = document.getElementById("navbar-header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+</script>
+        <div class="container-fluid">
+            <div class="row" >
+                <div class="col-lg-12"id="row">
+
+                    <div class="navbar-header" id="navbar-header">
                         <button class="navbar-toggle" data-target="#mobile_menu" data-toggle="collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-                        <a href="#" class="navbar-brand">TAMKINE.ORG</a>
+                        <a href="https://tamkine.org/fr/" class="navbar-brand">TAMKINE.ORG</a>
                     </div>
 
                     <div class="navbar-collapse collapse" id="mobile_menu">
                         <ul class="nav navbar-nav">
-                            <li class="active"><a href="#">Home</a></li>
+                            <li class=""><a href="#">Home</a></li>
                             <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <span class="caret"></span></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="#">About One</a></li>
@@ -55,149 +75,20 @@
             </div>
         </div>
     </div>
-    <div class="container">
-    <header>
-      
-      <style>
- body {
-  background: radial-gradient(circle, black, white);
-}
-
-</style>
-</header>
-</div>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Slideshow Images</title>
-    <style>
-      * {
-        box-sizing: border-box
-      }
-      body {
-        font-family: Verdana, sans-serif;
-        margin: 0
-      }
-      .mySlides {
-        display: none
-      }
-      img {
-        vertical-align: middle;
-      }
-      .slideshow-container {
-        max-width: 9000px;
-        position: relative;
-        margin: auto;
-      }
-      /* Next & previous buttons */
-      .prev,
-      .next {
-        cursor: pointer;
-        position: absolute;
-        top: 50%;
-        width: auto;
-        padding: 16px;
-        margin-top: -22px;
-        color: white;
-        font-weight: bold;
-        font-size: 18px;
-        transition: 0.6s ease;
-        border-radius: 0 3px 3px 0;
-        user-select: none;
-      }
-      /* Position the "next button" to the right */
-      .next {
-        right: 0;
-        border-radius: 3px 0 0 3px;
-      }
-      /* On hover, add a black background color with a little bit see-through */
-      .prev:hover,
-      .next:hover {
-        background-color: rgba(0, 0, 0, 0.8);
-      }
-      /* Caption text */
-      .text {
-        color: #ffffff;
-        font-size: 15px;
-        padding: 8px 12px;
-        position: absolute;
-        bottom: 8px;
-        width: 100%;
-        text-align: center;
-      }
-      /* Number text (1/3 etc) */
-      .numbertext {
-        color: #ffffff;
-        font-size: 12px;
-        padding: 8px 12px;
-        position: absolute;
-        top: 0;
-      }
-      /* The dots/bullets/indicators */
-      .dot {
-        cursor: pointer;
-        height: 15px;
-        width: 15px;
-        margin: 0 2px;
-        background-color: #999999;
-        border-radius: 50%;
-        display: inline-block;
-        transition: background-color 0.6s ease;
-      }
-      .active,
-      .dot:hover {
-        background-color: #111111;
-      }
-      /* Fading animation */
-      .fade {
-        -webkit-animation-name: fade;
-        -webkit-animation-duration: 1.5s;
-        animation-name: fade;
-        animation-duration: 1.5s;
-      }
-      @-webkit-keyframes fade {
-        from {
-          opacity: .4
-        }
-        to {
-          opacity: 1
-        }
-      }
-      @keyframes fade {
-        from {
-          opacity: .4
-        }
-        to {
-          opacity: 1
-        }
-      }
-      /* On smaller screens, decrease text size */
-      @media only screen and (max-width: 300px) {
-        .prev,
-        .next,
-        .text {
-          font-size: 11px
-        }
-      }
-    </style>
-  </head>
-  <body>
+    
+    </header>
+    <body>
     <div class="slideshow-container">
       <div class="mySlides fade">
-        <div class="numbertext">1 / 3</div>
+        
         <img src="https://tamkine.org/wp-content/uploads/2020/10/IMG_8966.jpg" style="width:100%">
-        <div class="text">London, Ebgland</div>
+        
       </div>
       <div class="mySlides fade">
-        <div class="numbertext">2 / 3</div>
-        <img src="/uploads/media/default/0001/03/b7d624354d5fa22e38b0ab1f9b905fb08ccc6a05.jpeg" style="width:100%">
-        <div class="text">Sunset in Romania</div>
-      </div>
-      <div class="mySlides fade">
-        <div class="numbertext">3 / 3</div>
-        <img src="/uploads/media/default/0001/03/5bfad15a7fd24d448a48605baf52655a5bbe5a71.jpeg" style="width:100%">
-        <div class="text">New York, USA</div>
-      </div>
+        
+        <img src="https://www.unicef.org/philippines/sites/unicef.org.philippines/files/styles/hero_desktop/public/UNIPH2019008.jpg?itok=CnZKe5zQ" style="width:100%">
+        
+      
       <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
       <a class="next" onclick="plusSlides(1)">&#10095;</a>
     </div>
@@ -224,10 +115,105 @@
         setTimeout(showSlides, 2000); // Change image every 2 seconds
       }
     </script>
-  </body>
-</html>
+    
+ <div class="container">
+ <div class="row">
+   <h3><p style="text-align:center"><b>PRÉSENTATION</b></p></h3>
+   <p>&nbsp;</p><p>&nbsp;</p>
+  <img src="https://tamkine.org/wp-content/uploads/2020/10/abdelilah-kadili.jpg" alt="Pineapple" style="width:400px;height:235px;margin-right:50px;">
+  <div ><h4><p> La Fondation Tamkine est une Organisation Non Gouvernementale à But Non Lucratif. Créée le : 29/10/2015
 
+Le Comité Exécutif de la Fondation Tamkine pour l’Excellence et la Créativité est composé de professeurs universitaires, de spécialistes du secteur de la recherche et de la formation scientifiques, de cadres supérieures de l’administration publique, d’anciens responsables de l’éducation nationale, de militants consacrés à l’amélioration de la qualité de l’éducation, de dirigeants d’entreprises publiques et privées, de spécialistes des Technologies de l’Information et de la Communication,
+ toutes et tous justifiant d’une grande expérience et d’une expertise avérée dans le domaine.</p></h4></div>
 
+   <p>&nbsp;</p><p>&nbsp;</p>
+   <p></p>
+ </div>
+<div><h3><p style="text-align:center"><b>NOS ACTUALITÉS</b></p></h3></div>
+<script href="{{ asset('js\owl.carousel.js') }}"></script>
+<div class="owl-carousel owl-theme">
+    <div class="item"><h4>1</h4></div>
+    <div class="item"><h4>2</h4></div>
+    <div class="item"><h4>3</h4></div>
+    <div class="item"><h4>4</h4></div>
+    <div class="item"><h4>5</h4></div>
+    <div class="item"><h4>6</h4></div>
+    <div class="item"><h4>7</h4></div>
+    <div class="item"><h4>8</h4></div>
+    <div class="item"><h4>9</h4></div>
+    <div class="item"><h4>10</h4></div>
+    <div class="item"><h4>11</h4></div>
+    <div class="item"><h4>12</h4></div>
+</div>
+<script>
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})</script>
+    </div>
+    </body>
+    <p>&nbsp;</p><p>&nbsp;</p>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+<footer>
+<div class="">
+   <div class="article">
+     <div class="left box">
+       <div class="upper">
+         <div class="topic">About us</div>
+         <p>"PakaInfo was founded in January 2018 with the aim of sharing a place for General Information languages articles.</p>
+       </div>
+       <div class="lower">
+         <div class="topic">Contact us</div>
+         <div class="phone">
+           <a href="#"><i class="fas fa-phone-volume"></i>+007 9898 015*</a>
+         </div>
+         <div class="email">
+           <a href="#"><i class="fas fa-envelope"></i>yourgmaailname@gmail.com</a>
+         </div>
+       </div>
+     </div>
+     <div class="middle box">
+       <div class="topic">Our Services</div>
+       <div><a href="#">Software source aPI, Development</a></div>
+       <div><a href="#">Software dev Source code, Reasearch</a></div>
+       <div><a href="#">Software User Interface Source code</a></div>
+       <div><a href="#">Laravel Development, Source code</a></div>
+       <div><a href="#">Mobile Application Source code</a></div>
+       <div><a href="#">Blogging & Earning</a></div>
+     </div>
+     <div class="right box">
+       <div class="topic">Subscribe us</div>
+       <form action="#">
+         <input type="text" placeholder="Enter Your email address">
+         <input type="submit" name="" value="Send">
+         <div class="media-icons">
+           <a href="#"><i class="fab fa-facebook-f"></i></a>
+           <a href="#"><i class="fab fa-instagram"></i></a>
+           <a href="#"><i class="fab fa-twitter"></i></a>
+           <a href="#"><i class="fab fa-youtube"></i></a>
+           <a href="#"><i class="fab fa-linkedin-in"></i></a>
+         </div>
+       </form>
+     </div>
+   </div>
+   <div class="bottom">
+     <p>Copyright © 2022 <a href="#">Pakainfo</a> All rights reserved</p>
+   </div>
+ </footer>
+ </div>
+    </div>
+</html>
